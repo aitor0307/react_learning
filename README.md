af08890ebe6238992e25ed9d3a541b0dfa4bfc57
aitor0307 <gazta90@gmail.com>
2022-05-26 10:37:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-react-example-gwc1qm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hello-react-example-gwc1qm)
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+ReactDOM.render(
+  <h1>Hello, React!</h1>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <h1>Everybody, everybody, it's getting to it, get started! Let's get started in here!</h1>,
+  document.getElementById('test')
+);
